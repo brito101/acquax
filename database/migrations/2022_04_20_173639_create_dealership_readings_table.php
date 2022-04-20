cf9b678d2ba2871e4dc956e6d7835370961de3cd
brito101 <rodrigobrito101@hotmail.com>
2022-04-20 19:23:09 -0300

    AGUARDANDO CONFIRMAÇÃO DO REGISTO

--- database/migrations/2022_04_20_173639_create_dealership_readings_table.php
+++ database/migrations/2022_04_20_173639_create_dealership_readings_table.php
@@ -15,6 +15,31 @@ class CreateDealershipReadingsTable extends Migration
     {
         Schema::create('dealership_readings', function (Blueprint $table) {
             $table->id();
+            $table->date('date_reading');                               //data da leitura (dados da fatura da concessionária)
+            $table->date('date_next_reading');                          //data da próxima leitura (dados da fatura da concessionária)
+            $table->integer('total_days');                              //total de dias faturados pela concessionária
+            $table->decimal('dealership_consumption', 13, 3);           //consumo em m3 medido pela concessionária
+            $table->decimal('dealership_consumption_tax_1', 13, 3);     //consumo em m3 medido pela concessionária para a primeira faixa
+            $table->decimal('dealership_consumption_tax_2', 13, 3);     //consumo em m3 medido pela concessionária para a segunda faixa
+            $table->decimal('monthly_consumption', 13, 3);              //consumo em m3 medido pela empresa de individualização
+            $table->decimal('water_value_consumption', 12, 2);          //valor da água (dados da fatura da concessionária)
+            $table->decimal('sewage_value_consumption', 12, 2);         //valor do esgoto (dados da fatura da concessionária)
+            $table->decimal('regulation_tax', 12, 2);                   //taxa de regulação (dados da fatura da concessionária caso exista)
+            $table->decimal('dealership_cost_tax_1', 13, 3);            //valor do m3  da concessionária da primeira faixa
+            $table->decimal('dealership_cost_tax_2', 13, 3);            //valor do m3  da concessionária da segunda faixa
+            $table->foreignId('dealership_id')                          //concessionária
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            /** Pattern */
+            $table->unsignedBigInteger('editor');
+            $table->foreign('editor')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade')
+                ->comment('Último editor');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
