f72c441ebfaa6022390822c4a510c80a8bea3a9c
brito101 <rodrigobrito101@hotmail.com>
2022-06-21 18:32:01 -0300

    ADD Purge CSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require("laravel-mix");
+require('laravel-mix-purgecss');
 
 /*
  |--------------------------------------------------------------------------
@@ -39,4 +40,5 @@ mix.js("resources/js/app.js", "public/js")
     .options({
         processCssUrls: false,
     })
-    .sourceMaps();
+    .sourceMaps()
+    .purgeCss();
